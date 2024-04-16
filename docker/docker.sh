f4a1aff31eff94595f75f4fa92ae5b9f26fcf767
Bogdan Drozdowski <>
2024-04-16 23:38:47 +0200

    Quote arguments in shell

--- docker/docker.sh
+++ docker/docker.sh
@@ -16,7 +16,7 @@ read -s pg_trinventum_pwd
 echo ''
 
 # NOTE: the network must be the same; set some reasonable hostname
-docker run -d --name trin-db --network trinventum_net --hostname trinventum-db -e POSTGRES_PASSWORD=$pg_master_pwd -e PGPWD=$pg_trinventum_pwd trinventum-db:latest
+docker run -d --name trin-db --network trinventum_net --hostname trinventum-db -e POSTGRES_PASSWORD="$pg_master_pwd" -e PGPWD="$pg_trinventum_pwd" trinventum-db:latest
 docker run -d -p 80:80 -p 443:443 --name trin-app --network trinventum_net --hostname trinventum-www trinventum-app:latest
 
 sed -i "s/POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$pg_master_pwd/" docker/docker-compose.yaml
